1ffe5c1b89e4d9de18214c8e6aabfbbb0f4401cf
Trudethath <bartekkuleta21@gmail.com>
2022-06-06 14:58:24 +0200

    add slider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { BrowserRouter } from "react-router-dom"
 import "./css/style.css"
 import Navbar from "./Navbar/Navbar"
 import Content from "./Content/Content"
+import AppProvider from "./AppProvider"
 
 function App() {
   return (
     <>
       <BrowserRouter basename='/'>
-        <Navbar />
-        <Content />
+        <AppProvider>
+          <Navbar />
+          <Content />
+        </AppProvider>
       </BrowserRouter>
     </>
   )
--- src/Content/Home.js
+++ src/Content/Home.js
@@ -1,4 +1,6 @@
+import { useContext } from "react"
 import SimpleImageSlider from "react-simple-image-slider"
+import { AppContext } from "../AppProvider"
 import image1 from "../images/1.png"
 import image2 from "../images/2.png"
 import image3 from "../images/3.png"
@@ -16,14 +18,21 @@ const images = [
 ]
 
 function Home() {
+  const { items } = useContext(AppContext)
+  // this year's collection
+  const newestItems = items.filter((item) => {
+    console.log(item)
+    return item.release_year === new Date().getFullYear()
+  })
+
   return (
     <div className='slider'>
       <SimpleImageSlider
         width={1000}
         height={500}
         images={images}
-        showBullets={true}
-        showNavs={true}
+        showBullets={false}
+        showNavs={false}
         autoPlay={true}
         autoPlayDelay={3}
       />
