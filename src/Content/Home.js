512267b0c88289c652ab3e10cdd18a45f045a89b
Trudethath <bartekkuleta21@gmail.com>
2022-06-06 14:13:45 +0200

    add home slider

--- src/Content/Home.js
+++ src/Content/Home.js
@@ -1,5 +1,34 @@
+import SimpleImageSlider from "react-simple-image-slider"
+import image1 from "../images/1.png"
+import image2 from "../images/2.png"
+import image3 from "../images/3.png"
+
+const images = [
+  {
+    url: image1,
+  },
+  {
+    url: image2,
+  },
+  {
+    url: image3,
+  },
+]
+
 function Home() {
-  return "home"
+  return (
+    <div className='slider'>
+      <SimpleImageSlider
+        width={1000}
+        height={500}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+        autoPlay={true}
+        autoPlayDelay={3}
+      />
+    </div>
+  )
 }
 
 export default Home
